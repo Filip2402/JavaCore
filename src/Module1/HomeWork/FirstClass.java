a9fc02fe17a2f4b089a78fb68a010c1b9112e163
PC <filprotsenko@gmail.com>
2016-11-01 14:04:38 +0200

    Intin commit

---
+++ src/Module1/HomeWork/FirstClass.java
@@ -0,0 +1,10 @@
+package Module1.HomeWork;
+
+/**
+ * Created by PC on 01.11.2016.
+ */
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("Мой первый класс на Java");
+    }
+}
---
+++ src/Module1/test.java
@@ -0,0 +1,7 @@
+package Module1;
+
+/**
+ * Created by PC on 01.11.2016.
+ */
+public class test {
+}
